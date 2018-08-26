2a383692657ea14ff2fdaacb0cea916d7814872c
thmiv <thmiv4@gmail.com>
2018-08-26 14:08:09 -0500

    some read functionality

---
+++ bamazonCustomer.js
@@ -0,0 +1,92 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+
+var connection = mysql.createConnection({
+  host: "35.188.44.144",
+  // Your port; if not 3306
+  port: 3306,
+  // Your username
+  user: "utcbc",
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  // execute function below
+    //createProduct("Stylus Pen", "Accessories", "500000", "25");
+    readProducts();
+});
+
+function createProduct(newItem, newDept, newPrice, newStock) {
+  console.log("Inserting a new product...\n");
+  var query = connection.query(
+    "INSERT INTO products SET ?",
+    {
+      product_name: newItem,
+      department_name: newDept,
+      customer_price: newPrice,
+      stock_quantity: newStock
+    },
+    function(err, res) {
+      console.log(res.affectedRows + " product inserted!\n");
+      // Call updateProduct AFTER the INSERT completes
+      //updateProduct();
+      readProducts();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function updateProduct() {
+  console.log("Updating all Rocky Road quantities...\n");
+  var query = connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        stock_quantity: 100
+      }
+    ],
+    function(err, res) {
+      console.log(res.affectedRows + " products updated!\n");
+      // Call deleteProduct AFTER the UPDATE completes
+      //deleteProduct(itemRemove);
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function deleteProduct(itemRemove) {
+  console.log("Deleting item...\n");
+  connection.query(
+    "DELETE FROM products WHERE ?",
+    {
+      product_name: itemRemove
+    },
+    function(err, res) {
+      console.log(res.affectedRows + " products deleted!\n");
+      // Call readProducts AFTER the DELETE completes
+      //readProducts();
+    }
+  );
+}
+
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    for (i = 0; i < res.length; i++) {
+        console.log("Product: " + res[i].product_name + ", Dept: " + res[i].department_name + ", Price: $" + res[i].customer_price + ", Quantity: " + res[i].stock_quantity);
+    }
+
+    connection.end();
+  });
+}
