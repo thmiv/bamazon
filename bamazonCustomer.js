2697cdd60dc07c481961f7a6b55594552fada6da
thmiv <thmiv4@gmail.com>
2018-08-28 01:34:07 -0500

    functioning bamazon customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,13 +13,111 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  // execute function below
-    //createProduct("Stylus Pen", "Accessories", "500000", "25");
-    customerPurchase();
+  // execute database function below
+  //createProduct("Stylus Pen", "Accessories", "500000", "25");
+  customerPurchase();
 });
+
+function readProducts() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    for (i = 0; i < res.length; i++) {
+      console.log(chalk.green(res[i].item_id) +" "+ chalk.yellow(res[i].product_name) + ", Dept: " + chalk.blue(res[i].department_name) + ", Price: $" + chalk.cyanBright(res[i].customer_price) + ", Quantity: " + chalk.magenta(res[i].stock_quantity));
+    }
+    console.log(" ");
+  });
+}
+
+function customerPurchase() {
+  readProducts();
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) { throw err };
+
+    inquirer.prompt([
+      {
+        name: "userChoice",
+        type: "rawlist",
+        message: "What would you like to purchase?",
+        choices: function () {
+          var productArray = [];
+          for (i = 0; i < res.length; i++) {
+            productArray.push(res[i].product_name);
+          }
+          return productArray;
+        }
+      },
+      {
+        name: "answerAmount",
+        type: "input",
+        message: "Please enter in the amount you would like to buy: "
+      }
+    ]).then(function (answer) {
+      //console.log(answer.userChoice);
+      if (Number.isInteger(parseInt(answer.answerAmount)) && (parseInt(answer.answerAmount) > 0)) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].product_name === answer.userChoice) {
+            chosenItem = res[i];
+          }
+        }
+        // determine it is available
+        if ((chosenItem.stock_quantity - parseInt(answer.answerAmount)) >= 0) {
+          var purchaseCost = chosenItem.customer_price * parseInt(answer.answerAmount);
+          // bid was high enough, so update db, let the user know, and start over
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: (chosenItem.stock_quantity - parseInt(answer.answerAmount))
+              },
+              {
+                item_id: chosenItem.item_id
+              }
+            ],
+            function (error) {
+              if (error) { throw err };
+              console.log("\nItem(s) bought successfully! Total cost: $" + chalk.red(purchaseCost));
+              shopAgain();
+            }
+          );
+        } else {
+          // quantity wasn't high enough, so apologize and start over
+          console.log("\nSorry, there is not enough available. Please try again...");
+          shopAgain();
+        }
+      } else {
+        console.log("Sorry, please enter an integer amount greater than zero...");
+        shopAgain();
+      }
+    });
+  });
+}
+
+function shopAgain() {
+  console.log(" ");
+  inquirer.prompt([
+    {
+      name: "userShop",
+      type: "confirm",
+      message: "Would you like to continue shopping?",
+      default: true
+    }
+  ]).then(function(answer) {
+    if (answer.userShop) {
+      console.log(" ");
+      customerPurchase();
+    } else {
+      connection.end();
+    }
+  });
+}
+
+// useful functions **********************************************************************************************
 
 function createProduct(newItem, newDept, newPrice, newStock) {  // creates a new product and adds it to sqlDb
   console.log("Inserting a new product...\n");
@@ -31,33 +129,12 @@ function createProduct(newItem, newDept, newPrice, newStock) {  // creates a new
       customer_price: newPrice,
       stock_quantity: newStock
     },
-    function(err, res) {
+    function (err, res) {
       console.log(res.affectedRows + " product inserted!\n");
       // Call updateProduct AFTER the INSERT completes
       getProducts();
     }
   );
-
-  // logs the actual query being run
-  console.log(query.sql);
-}
-
-function updateProduct() {
-  console.log("Updating all Rocky Road quantities...\n");
-  var query = connection.query(
-    "UPDATE products SET ? WHERE ?",
-    [
-      {
-        stock_quantity: 100
-      }
-    ],
-    function(err, res) {
-      console.log(res.affectedRows + " products updated!\n");
-      // Call deleteProduct AFTER the UPDATE completes
-      //deleteProduct(itemRemove);
-    }
-  );
-
   // logs the actual query being run
   console.log(query.sql);
 }
@@ -69,52 +146,10 @@ function deleteProduct(itemRemove) {
     {
       product_name: itemRemove
     },
-    function(err, res) {
+    function (err, res) {
       console.log(res.affectedRows + " products deleted!\n");
       // Call readProducts AFTER the DELETE completes
       //readProducts();
     }
   );
-}
-
-function readProducts() {
-  console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    // Log all results of the SELECT statement
-    for (i = 0; i < res.length; i++) {
-        console.log("Product: " + res[i].product_name + ", Dept: " + res[i].department_name + ", Price: $" + res[i].customer_price + ", Quantity: " + res[i].stock_quantity);
-    }
-
-    connection.end();
-  });
-}
-
-function customerPurchase() {
-  var getItemsArrary = [];
-  connection.query("SELECT * FROM products", function(err, res) { 
-    if (err) { throw err };
-    var productArray = [];
-    var subProductArray = [];
-    for (i = 0; i < res.length; i++) {
-        subProductArray.push(res[i].product_name);
-        subProductArray.push(res[i].department_name);
-        subProductArray.push(res[i].customer_price);
-        subProductArray.push(res[i].stock_quantity);
-        productArray.push(subProductArray);
-        subProductArray = [];
-    }
-    getItemsArrary = productArray;
-    console.log(getItemsArrary);
-  });
-  
-  
-  // inquirer.prompt([{
-  //   name: "userChoice",
-  //   type: "rawlist",
-  //   message: "What would you like to purchase?",
-  //   choices: ["asdf", "gdfsa", "adafgea"] //getItemsArrary
-  // }]).then(function(answer) {
-  //   console.log(answer.userChoice);
-  // });
 }
